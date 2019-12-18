4310f8cee43fcc3125f5adb094bfd70bc82b0595
Agallio Samai <agalliosamai@Agallios-MacBook-Air.local>
2019-12-18 13:02:43 +0700

    Update bible/date endpoint

--- pages/api/bible/[date].js
+++ pages/api/bible/[date].js
@@ -58,25 +58,28 @@ export default async (req, res) => {
   }
 
   // Perjanjian Baru 1
-  pb1DashSplit = pb1SpaceSplit[1].split('-');
-  if (pb1DashSplit.length > 1) {
-    let place = 1;
-    for (i = Number(pb1DashSplit[0]); i <= pb1DashSplit[1]; i++) {
-      const passage = await BibleModel.find({
-        $and: [{ abbr: pb1SpaceSplit[0] }, { chapter: i }]
-      });
-      pb1Arr.push({
-        version: 'tb',
-        book: passage[0].book,
-        chapter: i,
-        passagePlace: `pb1`,
-        data: passage[0].verses
-      });
-    }
+  let pb1ColonSplit = pb1SpaceSplit[1].split(':');
+  if (pb1ColonSplit.length > 1) {
+    pb2DashSplit = pb1ColonSplit[1].split('-');
+    const passage = await BibleModel.find({
+      $and: [{ abbr: pb1SpaceSplit[0] }, { chapter: pb1ColonSplit[0] }]
+    });
+    pb1Arr.push({
+      version: 'tb',
+      book: passage[0].book,
+      chapter: passage[0].chapter,
+      passagePlace: `pb1`,
+      data: passage[0].verses.filter(
+        item =>
+          item.verse >= Number(pb2DashSplit[0]) &&
+          item.verse <= Number(pb2DashSplit[1])
+      )
+    });
   } else {
     const passage = await BibleModel.find({
       $and: [{ abbr: pb1SpaceSplit[0] }, { chapter: pb1SpaceSplit[1] }]
     });
+    console.log(passage);
     pb1Arr.push({
       version: 'tb',
       book: passage[0].book,
@@ -92,8 +95,9 @@ export default async (req, res) => {
     if (pb2ColonSplit.length > 1) {
       pb2DashSplit = pb2ColonSplit[1].split('-');
       const passage = await BibleModel.find({
-        $and: [{ abbr: pb2SpaceSplit[0] }, { chapter: pb2SpaceSplit[1] }]
+        $and: [{ abbr: pb2SpaceSplit[0] }, { chapter: pb2ColonSplit[0] }]
       });
+      console.log(passage);
       pb2Arr.push({
         version: 'tb',
         book: passage[0].book,
@@ -109,6 +113,7 @@ export default async (req, res) => {
       const passage = await BibleModel.find({
         $and: [{ abbr: pb2SpaceSplit[0] }, { chapter: pb2SpaceSplit[1] }]
       });
+      console.log(passage);
       pb2Arr.push({
         version: 'tb',
         book: passage[0].book,
@@ -121,6 +126,7 @@ export default async (req, res) => {
     const passage = await BibleModel.find({
       $and: [{ abbr: pb2SpaceSplit[0] }, { chapter: pb2SpaceSplit[1] }]
     });
+    console.log(passage);
     pb2Arr.push({
       version: 'tb',
       book: passage[0].book,
