7a9e4e59b5a12c457148fb358e45e8d8f6eaa306
agallio <agalliosamai@gmail.com>
2020-07-10 19:49:14 +0700

    Modify workbox options

--- next.config.js
+++ next.config.js
@@ -8,26 +8,30 @@ const nextConfig = {
   },
   target: 'serverless',
   transformManifest: (manifest) => ['/'].concat(manifest),
-  generateInDevMode: true,
   workboxOpts: {
-    swDest: 'static/service-worker.js',
+    swDest: process.env.NEXT_EXPORT
+      ? 'service-worker.js'
+      : 'static/service-worker.js',
     runtimeCaching: [
       {
         urlPattern: /^https?.*/,
         handler: 'NetworkFirst',
         options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
+          cacheName: 'offlineCache',
           expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60,
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
+            maxEntries: 200,
           },
         },
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/service-worker.js',
+        destination: '/_next/static/service-worker.js',
+      },
+    ]
   },
 }
 
