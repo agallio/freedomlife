c720a43d438303f9ef5c10c7408b5f9c1d7d93de
agallio <agalliosamai@gmail.com>
2020-10-23 14:28:26 +0700

    Update persembahan wording & styling

--- pages/persembahan.tsx
+++ pages/persembahan.tsx
@@ -39,8 +39,8 @@ const Persembahan = (): JSX.Element => {
       </Head>
 
       <div className="container" style={{ paddingBottom: 30 }}>
-        <h2 className="header-title" style={{ marginTop: 70 }}>
-          Persembahan
+        <h2 className="header-title" style={{ marginTop: 35 }}>
+          Persembahan &amp; Perpuluhan
         </h2>
         <p className="header-subtitle">
           Gereja Kristen Kemah Daud - Yogyakarta
@@ -50,15 +50,29 @@ const Persembahan = (): JSX.Element => {
       <div className="container-fluid">
         <Card className="styled-fluid-card">
           <CardContent className="persembahan" style={{ padding: '16px 10%' }}>
-            <p className="regular-text primary">
+            <p className="persembahan-text">
               Dukungan untuk pekerjaan pelayanan Gereja Kristen Kemah Daud
-              Yogyakarta dapat dilakukan melalui QRIS dibawah ini :
+              Yogyakarta dapat dilakukan melalui scan QRIS dibawah ini :
             </p>
 
             <img src="/images/qris.jpeg" alt="qris" style={{ width: '100%' }} />
+            <p className="persembahan-text" style={{ fontSize: 13 }}>
+              Melalui aplikasi m-Banking / Digital Payment seperti :
+            </p>
+            <div className="persembahan-block">
+              <b>
+                <i>Gopay, OVO, DANA &amp; LinkAja</i>
+              </b>
+            </div>
+            <p
+              className="persembahan-text"
+              style={{ fontSize: 13, textAlign: 'center' }}
+            >
+              (Bebas biaya administrasi untuk semua platform pembayaran)
+            </p>
 
             <hr />
-            <p className="regular-text primary">
+            <p className="persembahan-text">
               Atau dapat juga dilakukan melalui transfer pada rekening bank
               dibawah ini :
             </p>
@@ -73,7 +87,7 @@ const Persembahan = (): JSX.Element => {
             >
               8000-7752-1000
             </Button>
-            <p className="regular-text primary">
+            <p className="persembahan-text">
               a/n Gereja Kristen Kemah Daud (Klik Untuk Salin)
             </p>
             <h1 className="bold-text" style={{ margin: '10px 0' }}>
@@ -86,7 +100,7 @@ const Persembahan = (): JSX.Element => {
             >
               0600-311-611
             </Button>
-            <p className="regular-text primary">
+            <p className="persembahan-text">
               a/n Yay Pelita Bangsa (Klik Untuk Salin)
             </p>
           </CardContent>
