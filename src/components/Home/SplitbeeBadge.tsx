380397fc053e3b394e98c58e348463b464ad3495
agallio <agalliosamai@gmail.com>
2021-02-16 19:36:42 +0700

    Modify splitbee badge

--- src/components/Home/SplitbeeBadge.tsx
+++ src/components/Home/SplitbeeBadge.tsx
@@ -1,25 +1,30 @@
+import { useEffect, useState } from 'react'
 import { useTheme } from 'next-themes'
 
 const SplitbeeBadge: React.FC = () => {
+  const [isMounted, setIsMounted] = useState(false)
   const { theme } = useTheme()
+
+  useEffect(() => setIsMounted(true), [])
 
   return (
     <div className="flex align-center justify-center my-8">
-      {theme === 'dark' ? (
-        <a href="https://splitbee.io">
-          <img
-            src="https://splitbee-cdn.fra1.cdn.digitaloceanspaces.com/static/badge/splitbee-badge-dark.svg"
-            alt="Analytics by Splitbee.io"
-          />
-        </a>
-      ) : (
-        <a href="https://splitbee.io">
-          <img
-            src="https://splitbee-cdn.fra1.cdn.digitaloceanspaces.com/static/badge/splitbee-badge.svg"
-            alt="Analytics by Splitbee.io"
-          />
-        </a>
-      )}
+      {isMounted &&
+        (theme === 'dark' ? (
+          <a href="https://splitbee.io">
+            <img
+              src="https://splitbee-cdn.fra1.cdn.digitaloceanspaces.com/static/badge/splitbee-badge-dark.svg"
+              alt="Analytics by Splitbee.io"
+            />
+          </a>
+        ) : (
+          <a href="https://splitbee.io">
+            <img
+              src="https://splitbee-cdn.fra1.cdn.digitaloceanspaces.com/static/badge/splitbee-badge.svg"
+              alt="Analytics by Splitbee.io"
+            />
+          </a>
+        ))}
     </div>
   )
 }
