7d3ed46bd79b26b9df08e1163f91559e296ea354
agallio <me@agallio.xyz>
2022-02-25 14:05:48 +0700

    Add lang to html tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="id">
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <link
